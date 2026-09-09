77b7ba55678954ea96a5a21dad10404b89f8da7c
adhi <adhithiyan1977@gmail.com>
2026-09-09 09:30:27 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,30 @@
-import os , urllib.parse , urllib.request , render_template
-from app.youtube import youtube_bp
 
-Gemini_api_key = "Gemini_API_Key";
+from Flask import Blueprint , request , jsonify
 
-def home():
-  return render_template ("index.html")
+youtube_bp = Blueprint(
+  " youtube ",
+  __name__
+  )
 
-def create_app():
+@youtube_bp.route(
+  "/play" ,
+  methoda =["POST"]
 
-  app = Flask(_name_)
+  )
+  def play ():
 
-  app.register_blueprint(youtube_bp, url_prefix="/youtube")
+   data = request.get_json(
+     silent = True
+     )or {}
 
-@app.route("/html")
+     command = data.get(
+     "command",
+     ""
+     ).strip()
 
-  def html():
-     return render_template("index.html")
+     if not command : 
 
-return app;
+            return jsonify({
+              "success"="False",
+              "message"="there's no song name mentioned "
+              })400
